30ae7ecbba0303ddfc7709c8275a7b7599b02ed9
Victor Zanda <victorzanpra@gmail.com>
2025-08-01 12:02:03 +0200

    nuevo nombre app

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ progress_data = {
 }
 
 # Inicializar la app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title="Departamento Datos - Villarreal CF")
 app.config.suppress_callback_exceptions = True  # <-- Añade esta línea
 server = app.server
 
